dbef37c13659267d2767f1229186c2c6e79c2e98
Germangalia <germangalia@iesebre.com>
2016-02-16 16:54:55 +0100

    Add Notify class to recive notifications.

---
+++ app/src/main/java/com/example/ggalia84/onesignalnotification/Notify.java
@@ -0,0 +1,35 @@
+package com.example.ggalia84.onesignalnotification;
+
+/**
+ * Created by ggalia84 on 16/02/16.
+ */
+import android.app.Application;
+import android.util.Log;
+
+import com.onesignal.OneSignal;
+
+import org.json.JSONObject;
+
+public class Notify extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        OneSignal.startInit(this).init();
+    }
+
+    private class ExampleNotificationOpenedHandler implements OneSignal.NotificationOpenedHandler {
+        @Override
+        public void notificationOpened(String message, JSONObject additionalData, boolean isActive) {
+            try {
+                if (additionalData != null) {
+                    if (additionalData.has("actionSelected"))
+                        Log.d("OneSignalExample", "OneSignal notification button with id " + additionalData.getString("actionSelected") + " pressed");
+
+                    Log.d("OneSignalExample", "Full additionalData:\n" + additionalData.toString());
+                }
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        }
+    }
+}
